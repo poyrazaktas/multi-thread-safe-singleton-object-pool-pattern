0b6c2e8c372ca9bf98afd62627aeaa58fea71736
Poyraz Aktas <poyraz.educational@gmail.com>
2022-03-28 16:21:23 +0300

    init project

---
+++ src/main/java/com/poyrazaktas/multi_thread_safe_singleton_object_pool_pattern/Main.java
@@ -0,0 +1,7 @@
+package main.java.com.poyrazaktas.multi_thread_safe_singleton_object_pool_pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
